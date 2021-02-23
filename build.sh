8022300d720fed694866b78b32d9aa2ccc2c39c0
Minh Nguyen <tienminh.uet@gmail.com>
2021-02-23 18:23:01 +0900

    add gg analytics

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 docker run --rm \
+  --env JEKYLL_ENV=production \
   --volume="$PWD:/srv/jekyll" \
   -it minhnt95/jekyll_minh \
   jekyll build
--- serve.sh
+++ serve.sh
@@ -1,4 +1,5 @@
 docker run --rm \
+  --env JEKYLL_ENV=development \
   -p 4000:4000\
   --volume="$PWD:/srv/jekyll" \
   -it minhnt95/jekyll_minh \
